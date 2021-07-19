fa0abac69a79dce0ad12ff8eb0de1e9cbde5701c
senthils-aukbc <53608107+senthils-aukbc@users.noreply.github.com>
2021-07-19 12:28:23 +0530

    Setup initial skeleton of the app which runs without errors.

---
+++ index.py
@@ -0,0 +1,32 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+from apps import home, demography, religion, aboutus, contactus
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/apps/home':
+        return home.layout
+    elif pathname == '/apps/demography':
+        return demography.layout
+    elif pathname == '/apps/religion':
+        return religion.layout
+    elif pathname == '/apps/aboutus':
+        return aboutus.layout
+    elif pathname == '/apps/contactus':
+        return contactus.layout
+    else:
+        return '404'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
